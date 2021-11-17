164e0f4b2fe8aba710ac942c867635813736028d
Gordon <gordon_shih@paradise-soft.com.tw>
2021-11-17 16:46:55 +0800

    feat: 🎸 body and main image module

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import './App.css';
-import {BiMenu} from 'react-icons/bi'
-import {GiShoppingCart} from 'react-icons/gi'
+import { BiMenu } from 'react-icons/bi'
+import { GiShoppingCart } from 'react-icons/gi'
+import topImg from './images/topImg.png'
 
 
 const App = () => {
-  const SadDoge = ({sadDogeSay}) => {
+  const ImagesBlock = () => {
     return (
-      <div className="Body">
-        <img className="dogeSad" src={'https://cdn130.picsart.com/320648120422211.png?type=webp&to=min&r=640'}/>
-        <text>
-          {sadDogeSay}
-        </text>
+      <div className="imageBlockContainer">
+        <img className="imageBlockImg" src={'https://cdn130.picsart.com/320648120422211.png?type=webp&to=min&r=640'}/>
+        <img className="imageBlockImg" src={'https://cdn130.picsart.com/320648120422211.png?type=webp&to=min&r=640'}/>
       </div>
     )
   }
   return (
     <div className="Container">
       <header className="Header" >
-        <BiMenu style={{paddingLeft: 10}} color="#fff"/>
-        <text style={{color: '#fff'}}>
+        <BiMenu className="biMenu" color="#fff" />
+        <b style={{ color: '#fff' }}>
           mi.room
-        </text>
-        <GiShoppingCart style={{paddingRight: 10}} color="#fff"/>
+        </b>
+        <GiShoppingCart className="giShoppingCart" color="#fff" />
       </header>
-      <div className="topImage"/>
-      <div className="midTitle">
-        <text>
-          Mi casa es tu casa
-        </text>
+      <div className="mainBackground">
+        <div className="mainBody">
+          <img className="topImage" src={topImg} alt="topImage" />
+          <div className="midTitle">
+            <i>
+              Mi casa es tu casa
+            </i>
+          </div>
+          <div className="horizontalLine" />
+          <div className="verticalLine" />
+          <div className="contentBox" >
+            <ImagesBlock/>
+            <ImagesBlock/>
+            <ImagesBlock/>
+          </div>
+        </div>
       </div>
     </div>
   );
